9c6e9ac23ad983bd331d0546f5ede0b40beeee2a
damilolarandolph <damilolarandolph@gmail.com>
2021-05-04 19:00:57 +0000

    Implementing controller logic.

--- covpay/src/controllers/add-consumer-controller.php
+++ covpay/src/controllers/add-consumer-controller.php
@@ -1,14 +1,27 @@
 <?php
 require_once __DIR__ . "/../../../common/rest/controller.php";
+require_once __DIR__ . "/../data/repositories/consumer-repository.php";
 
 class AddConsumerController extends \Gateway\REST\Controller
 {
+
+    /** @var ConsumerRepository */
+    private $consumerRepo;
+    public function __construct()
+    {
+        $this->consumerRepo = new ConsumerRepository();
+    }
     public function get()
     {
         require_once __DIR__ . "/../views/add-consumer.php";
     }
     public function post()
     {
-        var_dump($_POST);
+        $consumer = new Consumer();
+        $consumer->bankAccount = $_POST['accountNumber'];
+        $consumer->bankBIC = $_POST['bankBIC'];
+        $consumer->apiSecret = bin2hex(random_bytes(32));
+        $this->consumerRepo->save($consumer);
+        header("Location: /consumers/add");
     }
 }
