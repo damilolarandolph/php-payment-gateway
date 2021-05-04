292aff02cdbe0bbb897c3ad90a848153aa6a3fa6
damilolarandolph <damilolarandolph@gmail.com>
2021-05-04 16:14:27 +0000

    Register add consumer route.

--- covpay/index.php
+++ covpay/index.php
@@ -1,2 +1,17 @@
 <?php
-echo "HEY I ALIVE";
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+require_once __DIR__ . '/../common/routing/router.php';
+require_once __DIR__ . "/src/controllers/home-controller.php";
+require_once __DIR__ . '/src/controllers/add-consumer-controller.php';
+
+use Gateway\Routing\Router;
+
+$router = new Router();
+
+$router->add("/consumers/add", new AddConsumerController());
+
+
+$router->navigate();
