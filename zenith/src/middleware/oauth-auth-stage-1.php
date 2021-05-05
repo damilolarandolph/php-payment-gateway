0dacb1e6b6f794016613707895d6e1de833d56ea
damilolarandolph <damilolarandolph@gmail.com>
2021-05-05 02:12:23 +0000

    Start Oauth logic.

---
+++ zenith/src/middleware/oauth-auth-stage-1.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . "/../data/repositories/consumer-repository.php";
+
+$headers = getallheaders();
+if (empty($headers['authorization'])) {
+    var_dump($headers);
+    http_response_code(401);
+    $message = new \stdClass();
+    $message->message = "AUTH_HEADER_MISSING";
+    echo json_encode($message);
+    die();
+}
+$authHeader = $headers['authorization'];
+$consumerRepo = new ConsumerRepository();
+
+$consumer = $consumerRepo->findById($authHeader);
+
+if (!$consumer) {
+    http_response_code(401);
+    echo json_encode(array('message' => "INVALID_CONSUMER"));
+    die();
+}
