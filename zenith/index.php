c551d92908da777396ecd5e6e5c5c675c8947123
damilolarandolph <damilolarandolph@gmail.com>
2021-05-07 20:15:01 +0000

    Added a migrate route.

--- zenith/index.php
+++ zenith/index.php
@@ -7,12 +7,14 @@ ini_set('display_errors', 1);
 error_reporting(-1);
 require __DIR__ . "/../common/routing/router.php";
 require_once __DIR__ . "/src/controllers/oauth.php";
+require_once __DIR__ . "/src/controllers/migration.php";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 
 $router = new Router();
 
 $router->post("/oauth/authorize", OauthController::class, 'authorize');
+$router->get("/migrate", MigrationController::class, 'migrate');
 
 
 $router->navigate();
