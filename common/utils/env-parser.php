82767a73134fb6895abdfb96e93bdc5dbf8ccb58
damilolarandolph <damilolarandolph@gmail.com>
2021-05-07 21:39:19 +0000

    Implemented .env parser and loader.

---
+++ common/utils/env-parser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Gateway\Util;
+
+use Error;
+
+abstract class EnvParser
+{
+
+    public static function load($filePath)
+    {
+        $file = file_get_contents($filePath);
+
+        if (!$file) {
+            throw new Error("Failed to load env file");
+        }
+        $lines = preg_split("/\r\n|\n|\r/", $file);
+
+        foreach ($lines as $line) {
+            $keyValues = explode('=', $line);
+            $key = trim($keyValues[0]);
+            $value = trim($keyValues[1]);
+            define($key, $value);
+        }
+    }
+}
