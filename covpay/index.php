152e6ee07c98e3198e436ace549578903784f224
damilolarandolph <damilolarandolph@gmail.com>
2021-05-07 21:43:39 +0000

    Load .env file, fix incorrect route path.

--- covpay/index.php
+++ covpay/index.php
@@ -5,13 +5,15 @@ error_reporting(-1);
 
 require_once __DIR__ . '/../common/routing/router.php';
 require_once __DIR__ . '/src/controllers/add-consumer-controller.php';
+require_once __DIR__ . "/../common/utils/env-parser.php";
 
 use Gateway\Routing\Router;
 
+\Gateway\Util\EnvParser::load(__DIR__ . '/src/.env');
 $router = new Router();
 
 $router->get("/consumers/add", AddConsumerController::class, 'show');
-$router->post("/consumer/add", AddConsumerController::class, 'create');
+$router->post("/consumers/add", AddConsumerController::class, 'create');
 
 
 $router->navigate();
