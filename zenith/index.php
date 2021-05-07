6fb1ac85a7b397421db1d47d5aa1af6ff872c24c
damilolarandolph <damilolarandolph@gmail.com>
2021-05-07 21:43:51 +0000

    Load .env file.

--- zenith/index.php
+++ zenith/index.php
@@ -8,9 +8,12 @@ error_reporting(-1);
 require __DIR__ . "/../common/routing/router.php";
 require_once __DIR__ . "/src/controllers/oauth.php";
 require_once __DIR__ . "/src/controllers/migration.php";
+require_once __DIR__ . "/../common/utils/env-parser.php";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 
+// Load env file
+\Gateway\Util\EnvParser::load(__DIR__ . "/src/.env");
 $router = new Router();
 
 $router->post("/oauth/authorize", OauthController::class, 'authorize');
