8517c525f9699ba453ac9ae11168edd3be952f19
damilolarandolph <damilolarandolph@gmail.com>
2021-05-09 18:34:11 +0000

    Added oauth dialog endpoint.

--- zenith/index.php
+++ zenith/index.php
@@ -5,18 +5,18 @@ use Gateway\Routing\Router;
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
+require_once __DIR__ . "/../common/utils/env-parser.php";
+\Gateway\Util\EnvParser::load(__DIR__ . "/src/.env");
 require __DIR__ . "/../common/routing/router.php";
 require_once __DIR__ . "/src/controllers/oauth.php";
 require_once __DIR__ . "/src/controllers/migration.php";
-require_once __DIR__ . "/../common/utils/env-parser.php";
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 
 // Load env file
-\Gateway\Util\EnvParser::load(__DIR__ . "/src/.env");
 $router = new Router();
-
 $router->post("/oauth/authorize", OauthController::class, 'authorize');
+$router->get("/oauth/dialog", OauthController::class, 'displayOtpConfirm');
 $router->get("/migrate", MigrationController::class, 'migrate');
 
 
