74be5c8e0797ebe5f15fd47d57658f144d7ff26f
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-14 02:20:04 +0000

    Reworked validation flow.

--- common/utils/field_checker.php
+++ common/utils/field_checker.php
@@ -7,8 +7,9 @@ function checkFields($assocArray, $fieldConfig)
     foreach ($fieldConfig as $field) {
         if (empty($assocArray[$field])) {
             http_response_code(400);
-            echo json_encode(array('message' => "Field $field is required", 'code' => "MISSING_FIELD"));
-            die();
+            return array('message' => "Field $field is required", 'code' => "MISSING_FIELD", 'field' => $field);
         }
     }
+
+    return true;
 }
