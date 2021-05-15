79b321a51036032658b96c3511dab6f68da3832f
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-15 23:04:24 +0000

    Added routes.

--- banknet/index.php
+++ banknet/index.php
@@ -1,2 +1,18 @@
 <?php
-echo "HEY I ALIVE";
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+require_once __DIR__ . "/../common/utils/env-parser.php";
+\Gateway\Util\EnvParser::load(__DIR__ . "/src/.env");
+require_once __DIR__ . "/src/controllers/messenging.php";
+require_once __DIR__ . "/src/controllers/migration.php";
+require_once __DIR__ . '/../common/routing/router.php';
+
+use Gateway\Routing\Router;
+
+$router = new Router();
+
+$router->get("/migrate", MigrationController::class, "migrate");
+$router->post("/sendmessage", MessengingController::class, "recieveMessage");
+
+$router->navigate();
