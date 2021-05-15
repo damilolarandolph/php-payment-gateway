85b53b7fa47b9e2221a1e214e2af825bb53eef7c
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-15 23:03:08 +0000

    Added database migration logic.

--- banknet/src/data/config/migrate.php
+++ banknet/src/data/config/migrate.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . "/../repositories/consumer-repository.php";
 
 try {
     $opt = array(
@@ -14,7 +15,18 @@ try {
     $sqlFile = file_get_contents(__DIR__ . "/banknet.sql");
     $dbConn->exec($sqlFile);
     echo "DATABASE MIGRATED SUCCESSFULLY";
+    $consumerRepo = new ConsumerRepository();
 
+    $zenith = new Consumer();
+    $zenith->name = "ZENITH";
+    $zenith->messengingEndpoint = "http://zenith.com/messenging";
+
+    $mastercard = new Consumer();
+    $mastercard->name = "MASTERCARD";
+    $mastercard->messengingEndpoint = "http://mastercard.com/messenging";
+
+    $consumerRepo->save($zenith);
+    $consumerRepo->save($mastercard);
     echo "DATABASE SEEDED SUCCESSFULLY";
 } catch (Exception $e) {
     echo "AN ERROR OCCURRED";
