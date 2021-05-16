3fbc6b39b6b382183c0d96b281b5c6d5f60c9103
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-16 21:14:29 +0000

    Remove uneeded validations.

--- zenith/src/middleware/jwt.php
+++ zenith/src/middleware/jwt.php
@@ -10,36 +10,15 @@ require_once __DIR__ . "/../../../common/utils/jwt.php";
 abstract class JWTMiddleware
 {
 
-    static function run()
+    static function run($token)
     {
 
         $revokedTokenRepo = new RevokedTokenRepository();
         $accountRepo = new BankAccountRepository();
 
-        $headers = getallheaders();
-        if (empty($headers['authorization'])) {
-            http_response_code(401);
-            $message = new \stdClass();
-            $message->message = "AUTH_HEADER_MISSING";
-            throw new Error("AUTH_HEADER_MISSING");
-        }
-
-        $authHeader  = $headers['authorization'];
-        $authHeaderParts = explode(' ', $authHeader);
-        if (count($authHeaderParts) !== 2) {
-            http_response_code(400);
-            throw new Error("AUTH_HEADER_WRONG_LENGTH");
-        }
-
-        list($bearer, $token) = $authHeaderParts;
-
-        if ($bearer !== 'Bearer') {
-            http_response_code(400);
-            throw new Error("BEARER_MISSING");
-        }
 
         $token = $revokedTokenRepo->findOne("WHERE token=?", $token);
-        if (!$token) {
+        if ($token) {
             http_response_code(401);
             throw new Error("TOKEN_REVOKED");
         }
