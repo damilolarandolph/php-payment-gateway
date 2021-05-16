68a4823aaeee5f385eaabe4447c0cb30e947ef06
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-16 20:23:28 +0000

    Fix repository bug.

--- mastercard/src/data/repositories/card-repository.php
+++ mastercard/src/data/repositories/card-repository.php
@@ -5,6 +5,7 @@ use Gateway\Data\ModelConfig;
 require_once __DIR__ . "/../../../../common/data/repository.php";
 require_once __DIR__ . "/../models/card.php";
 require_once __DIR__ . "/../config/connection.php";
+require_once __DIR__ . "/../../../../common/utils/aes-encrypt.php";
 
 /**
  * @method Card|false findById(string $id) 
@@ -13,7 +14,7 @@ class CardRepository extends \Gateway\Data\Repository
 {
     public function __construct()
     {
-        $modelConfig = new ModelConfig('cards', Consumer::class);
+        $modelConfig = new ModelConfig('cards', Card::class);
         foreach (get_class_vars($modelConfig->getClass()) as $key => $value) {
             if ($key == 'id') {
                 $modelConfig->addField($key, true);
