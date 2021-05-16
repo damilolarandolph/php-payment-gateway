9f6d4d89d9e1e101f22c054903c2cf7d0467f688
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-16 21:19:31 +0000

    Add route endpoints.

--- mastercard/index.php
+++ mastercard/index.php
@@ -1,2 +1,25 @@
 <?php
-echo "HEY I ALIVE";
+
+use Gateway\Routing\Router;
+
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+require_once __DIR__ . "/../common/utils/env-parser.php";
+\Gateway\Util\EnvParser::load(__DIR__ . "/src/.env");
+require __DIR__ . "/../common/routing/router.php";
+require_once __DIR__ . "/src/controller/migration-controller.php";
+require_once __DIR__ . "/src/controller/payment-controller.php";
+header("Content-Type: application/json");
+header("Cache-Control: no-cache");
+
+// Load env file
+$router = new Router();
+
+$router->get("/migrate", MigrationController::class, 'migrate');
+$router->post("/pay", PaymentController::class, 'pay');
+$router->post("/requestotp", PaymentController::class, 'requestOTP');
+
+
+
+$router->navigate();
