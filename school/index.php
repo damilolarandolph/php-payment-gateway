36921c7a97302c69935a18f7f3d631b5dc866b5d
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-17 19:37:39 +0000

    Add routes.

---
+++ school/index.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+require_once __DIR__ . "/../common/utils/env-parser.php";
+\Gateway\Util\EnvParser::load(__DIR__ . "/src/.env");
+require_once __DIR__ . '/../common/routing/router.php';
+require_once __DIR__ . "/src/controllers/home.php";
+require_once __DIR__ . "/src/controllers/product.php";
+require_once __DIR__ . "/src/controllers/student.php";
+require_once __DIR__ . "/src/controllers/migration.php";
+header("Cache-Control: no-cache");
+header("Content-Type: application/json");
+
+use Gateway\Routing\Router;
+
+$router = new Router();
+
+$router->get("/", HomeController::class, 'home');
+$router->get("/migrate", MigrationController::class, 'migrate');
+$router->get("/api/student", StudentController::class, "getStudent");
+$router->post("/api/student", StudentController::class, "createStudent");
+$router->post("/api/student/update", StudentController::class, "saveStudent");
+$router->get("/api/products", ProductController::class, "getProducts");
+$router->post("/api/product", ProductController::class, "createProduct");
+
+
+
+$router->navigate();
