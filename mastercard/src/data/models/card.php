2f54914755944d914eb5874e0d86e7e4fc6f2750
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-17 21:32:00 +0000

    Set card details on creation.

--- mastercard/src/data/models/card.php
+++ mastercard/src/data/models/card.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . "/../../../../common/utils/random_int.php";
 
 class Card
 {
@@ -9,4 +10,14 @@ class Card
     public $expiry;
     public $bank;
     public $account;
+
+    public function __construct()
+    {
+        $prefix = "53";
+        $prefix .= getRandomInts(0, 9, 14);
+        $this->number = $prefix;
+        $this->pin = getRandomInts(0, 9, 4);
+        $this->cvv = getRandomInts(0, 9, 3);
+        $this->expiry = "12/25";
+    }
 }
