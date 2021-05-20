4eb6944b4ffaaa2bbee47de4510fa0b4199b69fb
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-20 11:40:42 +0000

    Fixed bugs and polished code.

--- mastercard/src/controller/payment-controller.php
+++ mastercard/src/controller/payment-controller.php
@@ -24,9 +24,8 @@ class PaymentController
             echo $e->getMessage();
             die();
         }
-        $errors = checkFields($requestData, array("source"));
         try {
-            list($bank, $account) = $this->getSource($requestData["source"]);
+            list($bank, $account) = $this->getSource($requestData);
         } catch (Error $e) {
             echo $e->getMessage();
             die();
@@ -41,7 +40,7 @@ class PaymentController
             echo json_encode(array("status" => "fail", "message" => "OTP_REQUEST_FAILED"));
             die();
         } else {
-            echo json_encode(array("status" => "success"));
+            echo $response;
             die();
         }
     }
@@ -67,8 +66,8 @@ class PaymentController
         $destinationBank = null;
         $destinationAccount = null;
         try {
-            list($sourceBank, $sourceAccount) = $this->getSource($sourceOfFunds);
-            list($destinationBank, $destinationAccount) = $this->getSource($destination);
+            list($sourceBank, $sourceAccount) = $this->getSource($requestData['sourceOfFunds']);
+            list($destinationBank, $destinationAccount) = $this->getSource($requestData['destination']);
         } catch (Error $e) {
             echo $e->getMessage();
             die();
@@ -85,23 +84,25 @@ class PaymentController
         );
 
         if (!empty($otp)) {
+
             $message->message['otp'] = $otp;
         } else {
+
             if (empty($token)) {
                 http_response_code(401);
-                echo json_encode(array("status" => "fail", "message" => "AUTH_NEEDED"));
+                echo json_encode(array("status" => "fail", "message" => $requestData));
                 die();
             }
             $message->message['token'] = $token;
         }
 
-        extract(MessengingService::sendMessage($message));
-        if ($statusCode !== 200) {
+        $result = MessengingService::sendMessage($message);
+        if ($result['statusCode'] !== 200) {
             http_response_code(401);
-            echo json_encode(array("status" => "fail", "message" => "TRANSACTION_FAILED"));
+            echo json_encode($result['response']);
             die();
         } else {
-            echo json_encode(array("status" => "success"));
+            echo json_encode(array("status" => "success", "message" => $response));
             die();
         }
     }
@@ -112,14 +113,14 @@ class PaymentController
             $cardData = $sourceMap['card'];
             $cardErrors = checkFields($cardData, array("cvv", "number", "pin", "expiry"));
 
-
             if ($cardErrors !== true) {
                 throw new Error(
                     json_encode($cardErrors)
                 );
+                die();
             }
 
-            $card = $this->cardRepo->findOne("WEHRE number=?", $cardData['number']);
+            $card = $this->cardRepo->findOne("WHERE number=?", $cardData['number']);
 
             if (!$card) {
                 http_response_code(401);
@@ -127,15 +128,15 @@ class PaymentController
             }
 
             if (
-                ($card->pin != $card['pin'])
-                || ($card->cvv != $card['cvv'])
-                || ($card->expiry != $card['expiry'])
+                ($card->pin != $cardData['pin'])
+                || ($card->cvv != $cardData['cvv'])
+                || ($card->expiry != $cardData['expiry'])
             ) {
                 http_response_code(401);
                 throw new Error(json_encode(array('status' => 'fail', 'message' => "CARD_DETAILS_INVALID")));
             }
 
-            $dateTime = DateTime::createFromFormat("d/y", $card['expiry']);
+            $dateTime = DateTime::createFromFormat("d/y", $card->expiry);
             if (time() >= $dateTime->getTimestamp()) {
                 http_response_code(401);
                 throw new Error(json_encode(array('status' => 'fail', 'message' => "CARD_EXPIRED")));
@@ -149,17 +150,15 @@ class PaymentController
                 http_response_code(400);
                 echo json_encode(array('status' => 'fail', 'message' => "TRANSACTION_DETAILS_NEEDED"));
                 die();
-                $bankData = $sourceMap['bank'];
-                $bankErrors =  checkFields($bankData, array("bank", "account"));
-
-                if ($bankErrors !== true) {
-                    throw new Error(json_encode($bankErrors));
-                }
-
-                $destinationBank = $bankData['bank'];
-                $destinationAccount = $bankData['account'];
-                return array($bankData['bank'], $bankData['account']);
             }
+            $bankData = $sourceMap['bank'];
+            $bankErrors =  checkFields($bankData, array("bank", "account"));
+
+            if ($bankErrors !== true) {
+                throw new Error(json_encode($bankErrors));
+            }
+
+            return array($bankData['bank'], $bankData['account']);
         }
     }
 }
