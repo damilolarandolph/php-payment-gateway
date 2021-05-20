7f8f54a5c1c1b3a6c6293bcab59a1ffc5b494f79
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-20 11:41:25 +0000

    Update routes and set default headers.

--- covpay/index.php
+++ covpay/index.php
@@ -10,8 +10,11 @@ use Gateway\Routing\Router;
 \Gateway\Util\EnvParser::load(__DIR__ . '/src/.env');
 require_once __DIR__ . '/../common/routing/router.php';
 require_once __DIR__ . '/src/controllers/add-consumer-controller.php';
+require_once __DIR__ . '/src/controllers/payment.php';
 require_once __DIR__ . '/src/controllers/home.php';
 require_once __DIR__ . '/src/controllers/migration.php';
+header("Content-Type: application/json");
+header("Cache-Control: no-cache");
 
 
 $router = new Router();
@@ -19,7 +22,12 @@ $router = new Router();
 $router->get("/", HomeController::class, 'home');
 $router->post("/api/consumer", HomeController::class, 'createConsumer');
 $router->get("/api/consumer/continue", AddConsumerController::class, 'continueCreate');
+$router->post("/api/payment/start", PaymentController::class, 'create');
+$router->post("/api/payment/setcard", PaymentController::class, 'setCardDetails');
+$router->post("/api/payment/finish", PaymentController::class, 'confirmOtp');
+$router->get("/api/payment/details", PaymentController::class, 'getCheckoutInfo');
+$router->post("/api/payment/refund", PaymentController::class, 'refundTransaction');
+$router->get("/checkout", HomeController::class, 'checkout');
 $router->get("/migrate", MigrationController::class, "migrate");
-
 
 $router->navigate();
