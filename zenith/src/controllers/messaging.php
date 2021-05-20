71d407a2ee0ad7f96d2400820286bcf56a1c0448
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-20 11:43:48 +0000

    Polishing code and fixing bugs.

--- zenith/src/controllers/messaging.php
+++ zenith/src/controllers/messaging.php
@@ -6,6 +6,7 @@ require_once __DIR__ . "/../data/repositories/account-repository.php";
 require_once __DIR__ . "/../middleware/jwt.php";
 require_once __DIR__ . "/../services/messenging-service.php";
 require_once __DIR__ . "/../services/otp-service.php";
+require_once __DIR__ . "/../services/account-service.php";
 
 class MessagingController
 {
@@ -28,14 +29,18 @@ class MessagingController
             die();
         }
 
-        $messageType = $messageParsed['messageType'];
-
         extract($messageParsed);
         switch ($messageType) {
             case 'WITHDRAW':
                 $account = $this->bankRepo->findByAccountNumber($sourceAccount);
                 if (!empty($otp)) {
-                    $result =  OtpService::verifyOtp($account->phoneNumber, $otp);
+                    try {
+                        $result =  OtpService::verifyOtp('233' . $account->phoneNumber, $otp);
+                    } catch (Error $e) {
+                        http_response_code(401);
+                        echo json_encode(array("status" => "fail", "message" => "INVALID_OTP_OR_EXPIRED"));
+                        die();
+                    }
                     if (!$result) {
                         http_response_code(401);
                         echo json_encode(array("status" => "fail", "message" => "INVALID_OTP"));
@@ -44,9 +49,9 @@ class MessagingController
                 } else if (!empty($token)) {
                     try {
                         $result = JWTMiddleware::run($token);
-                        if (!$result['account'] != $account->accountNumber) {
+                        if ($result['payload']['account'] != $account->accountNumber) {
                             http_response_code(401);
-                            echo json_encode(array("status" => "fail", "message" => "ACCOUNT_MISMATCH"));
+                            echo json_encode(array("status" => "fail", "message" => $result));
                             die();
                         }
                     } catch (Error $e) {
@@ -56,20 +61,26 @@ class MessagingController
                 } else {
                     http_response_code(401);
                     echo json_encode(array("status" => "fail", "message" => "AUTH_NEEDED"));
+                    die();
                 }
-
-                $result = $this->accountService->withdraw($account, $amount, $destinationBank, $destinationAccount);
+                try {
+                    $result = $this->accountService->withdraw($account, $amount, $destinationBank, $destinationAccount);
+                } catch (Error $e) {
+                    echo json_encode(array("status" => "fail", "message" => $e->getMessage()));
+                    die();
+                }
                 if ($result) {
-                    echo json_encode(array("status" => "success"));
+                    echo json_encode(array("status" => "success", "message" => "SUCCESS"));
                     die();
                 } else {
                     echo json_encode(array("status" => "fail", "message" => "TRANSACTION_FAIL"));
                     die();
                 }
+                return json_encode(array("status" => "success"));
                 break;
             case "REQUEST_OTP":
                 $account = $this->bankRepo->findByAccountNumber($account);
-                OtpService::sendOtp($account);
+                OtpService::sendOtp('233' . $account->phoneNumber);
                 echo json_encode(array("status" => "success"));
                 die();
                 break;
